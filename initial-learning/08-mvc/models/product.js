26d1d96ad8689e1f686cab13b82c10acb5777bb7
Administrator <admin@example.com>
2020-06-07 00:21:26 +0200

    mvc app updated

--- initial-learning/08-mvc/models/product.js
+++ initial-learning/08-mvc/models/product.js
@@ -19,11 +19,15 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(ttle, imageUrl,  price, description) {
+        this.title = ttle;
+        this.imageUrl =imageUrl;
+        this.description = description;
+        this.price = price;
     }    
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -35,4 +39,11 @@ module.exports = class Product {
     static fetchAll(cb) {
         getProductsFromFile(cb);
     };
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
+    }
 };
--- initial-learning/08-mvc/routes/shop.js
+++ initial-learning/08-mvc/routes/shop.js
@@ -2,9 +2,15 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
+router.get('/checkout', shopController.geCheckout);
+router.get('/orders', shopController.getOrders);
+router.get('/products', shopController.getProducts);
+router.get('/products/:productId', shopController.getProduct);
 
 module.exports = router;
